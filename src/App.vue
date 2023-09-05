dddb32b4a6821b5efeb6ea2ada79918acf4f18db
jhordan.followize <jhordan.suzuki@followize.com.br>
2023-09-05 10:19:26 -0300

    create components

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+
+    <div class="column is-three-quarter">
+      <TaskForm  />
+    </div>
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import TaskForm  from './components/TaskForm .vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    TaskForm 
+  }
+});
+</script>
+
+<style></style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <img src="../assets/logo.png" alt="">
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BarraLateral'
+});
+</script>
+
+<style scoped>
+header {
+    padding: 15px;
+    background: #0d3b66;
+    width: 50%;
+    height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
